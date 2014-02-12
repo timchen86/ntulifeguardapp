ab60a5049f74572077810a536594599c4dac2e41
ctf <tim.chen.86@gmail.com>
2014-02-12 15:14:54 +0800

    loginForm, tz to taipei, language to zh-tw

--- ntulgapp/views.py
+++ ntulgapp/views.py
@@ -1,24 +1,27 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django import forms
 
-class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField()
-    sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
+USER_INPUT_LEN_MIN = 1
+USER_INPUT_LEN_MAX = 100
+
+class loginForm(forms.Form):
+    login_id = forms.CharField(label=u'帳號', help_text=u'你的身份證字號', max_length=USER_INPUT_LEN_MAX)
+    login_pw = forms.CharField(label=u'密碼', max_length=USER_INPUT_LEN_MAX)
 
 
-def home(request):
+def login(request):
     if request.method == 'POST': # If the form has been submitted...
-        form = ContactForm(request.POST) # A form bound to the POST data
+        form = loginForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             # Process the data in form.cleaned_data
             # ...
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
+            #return HttpResponseRedirect('/thanks/') # Redirect after POST
+            return HttpResponse('thanks')
     else:
-        form = ContactForm() # An unbound form
+        form = loginForm() # An unbound form
 
     return render(request, 'home.html', {
         'form': form,
