b49c1e1fe7682efc3fc3b8777fa70b3645dbd8f0
ctf <tim.chen.86@gmail.com>
2014-02-12 18:26:30 +0800

    ntulguser signup form

--- ntulgapp/views.py
+++ ntulgapp/views.py
@@ -4,14 +4,17 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django import forms
 from django.contrib.auth import authenticate, login
-from ntulgapp.user import ntulgUser
+import logging
+from ntulgapp.user import ntulgUserForm
 
 USER_INPUT_LEN_MIN = 1
 USER_INPUT_LEN_MAX = 100
 
+logger = logging.getLogger(__name__)
+
 class loginForm(forms.Form):
-    login_id = forms.CharField(label=u'帳號', help_text=u'你的身份證字號', max_length=USER_INPUT_LEN_MAX)
-    login_pw = forms.CharField(label=u'密碼', max_length=USER_INPUT_LEN_MAX)
+    login_id = forms.CharField(required=False, label=u'帳號(account)', help_text=u'你的身份證字號(your ID)', max_length=USER_INPUT_LEN_MAX)
+    login_pw = forms.CharField(required=False, label=u'密碼(password)', max_length=USER_INPUT_LEN_MAX)
 
 class signupForm(forms.Form):
     pass
@@ -22,21 +25,26 @@ def signup_view(request):
 def login_view(request):
     if request.method == 'POST': # If the form has been submitted...
         form = loginForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            login_id = request.POST['login_id']
-            login_pw = request.POST['login_pw']
+        post_keys = request.POST.keys()
+        if u"signin" in post_keys:
+            if form.is_valid(): # All validation rules pass
+                login_id = request.POST['login_id']
+                login_pw = request.POST['login_pw']
 
-            user = authenticate(username=login_id, password=login_pw)
+                user = authenticate(username=login_id, password=login_pw)
 
-            if user is not None:
-                if user.is_active:
-                    #login(login_id, login_pw)
-                    return HttpResponse('ok login') 
-            else:
-                return HttpResponse('bad login')
+                if user is not None:
+                    if user.is_active:
+                        return HttpResponse('ok login') 
+                else:
+                    return HttpResponse('bad login')
+        elif u"signup" in post_keys:
+            #return HttpResponse('sign up')
+            return render(request, 'signup.html', {
+                'form': ntulgUserForm})
+
     else:
         form = loginForm() # An unbound form
-
     return render(request, 'home.html', {
         'form': form,
     })
