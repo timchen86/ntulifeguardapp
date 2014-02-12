166d08339c27147ad17abfdacfca6318d1dbd96a
ctf <tim.chen.86@gmail.com>
2014-02-12 14:47:07 +0800

    initial import

---
+++ ntulgapp/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django import forms
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField()
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
+
+
+def home(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    return render(request, 'home.html', {
+        'form': form,
+    })
---
+++ settings.py
@@ -0,0 +1,1 @@
+from ntulgapp.settings import *
