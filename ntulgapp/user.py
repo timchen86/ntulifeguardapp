1378d40b5e52c1895a2e51938128fac47700b529
ctf <tim.chen.86@gmail.com>
2014-02-13 15:48:37 +0800

    able to commit to db

--- ntulgapp/user.py
+++ ntulgapp/user.py
@@ -14,28 +14,28 @@ INPUT_MAX = 50
 MOBILE_LEN = 10
 
 class ntulgUser(models.Model):
-    user = models.OneToOneField(User)
-    stage_no = models.DecimalField(max_digits=10)#, help_text="cm")
+    stage_no = models.DecimalField(max_digits=10, decimal_places=0, verbose_name=u"救生班期數(stage no.)", help_text=u"非水協期數") #, help_text="cm")
+    cap_no = models.DecimalField(max_digits=3, decimal_places=0, verbose_name=u"泳帽號碼(cap no.)", blank=True, help_text=u"非必填(optional)")#, help_text="cm")
     name_cht = models.CharField(verbose_name=u"中文姓名(chinese name)", max_length=INPUT_MAX) 
-    name_eng = models.CharField(verbose_name=u"英文姓名(english name)", max_length=INPUT_MAX, help_text=u"格式:LI,DAFA(李,大發)(姓放前面,然後逗點,然後名)(不要使用\'-\'當分隔符號) <a href=\"http://www.englishname.org/\" target=\"_blank\">姓名英譯查詢</a>", validators=[RegexValidator(r'^[a-zA-Z ]+,[a-zA-Z ]+$')]) 
+    name_eng = models.CharField(verbose_name=u"英文姓名(english name)", max_length=INPUT_MAX, help_text=u"格式:LI, DAFA(李,大發)(姓放前面,然後逗點,然後名)(不要使用\'-\'當分隔符號) <a href=\"http://www.englishname.org/\" target=\"_blank\">姓名英譯查詢</a>", validators=[RegexValidator(r'^[a-zA-Z ]+,[a-zA-Z ]+$')]) 
     birthday = models.DateField(verbose_name=u"生日(birthday)",max_length=10, help_text="格式:西元年-月-日,例如:1980-2-28(format: year-month-day)")
     tel_mobile= models.CharField(verbose_name=u"手機號碼(mobile)", help_text=u"不要輸入數字以外的符號(don't enter anything other than numbers)",max_length=MOBILE_LEN, validators=[RegexValidator(r'^09[0-9]{8}$'),MinLengthValidator(MOBILE_LEN),MaxLengthValidator(MOBILE_LEN)])#, min_length=10)
-    tel = models.CharField(verbose_name=u"電話(tel no.)", blank=True, max_length=INPUT_MAX, help_text=u"格式:(xx)xxxxxxxxx, 非必填(not required)", validators=[RegexValidator(r'^\(?[0-9]*\)\s*[0-9]*$'),MinLengthValidator(8),MaxLengthValidator(14)]) 
-    email = models.EmailField(verbose_name=u"Email", max_length=INPUT_MAX)
+    tel = models.CharField(verbose_name=u"電話(telephone no.)", blank=True, max_length=INPUT_MAX, help_text=u"格式:(xx)xxxxxxxxx, 非必填(optional)", validators=[RegexValidator(r'^\(?[0-9]*\)\s*[0-9]*$'),MinLengthValidator(8),MaxLengthValidator(14)]) 
+    email = models.EmailField(verbose_name=u"Email", max_length=INPUT_MAX, help_text="請填寫正確，你管理系統的密碼將會寄到這個Email")
     address = models.CharField(verbose_name=u"通訊地址(address)", help_text=u"請加上五碼郵遞區號(don't forget the ZIP code)", max_length=INPUT_MAX)
     nationality = models.CharField(verbose_name=u"國籍(nationality)", choices=COUNTRY.items(), default="TW", max_length=INPUT_MAX)
-    identify_number = models.CharField(verbose_name=u"身分證字號(id)", help_text=u"外籍人士請填居留證號碼(.........)", max_length=INPUT_MAX, validators=[RegexValidator(r'(^[a-zA-Z][12][0-9]{8}$|^[a-zA-Z][cCdD][0-9]{8}$)'),MinLengthValidator(10),MaxLengthValidator(10)])
+    identify_number = models.CharField(verbose_name=u"身分證字號(ID.)", help_text=u"這將會是你登入管理系統的帳號，外籍人士請填居留證號碼(this is your account name to login to the management system)", max_length=10, validators=[RegexValidator(r'(^[a-zA-Z][12][0-9]{8}$|^[a-zA-Z][cCdD][0-9]{8}$)'),MinLengthValidator(10),MaxLengthValidator(10)])
     sex = models.CharField(verbose_name="性別(sex)", choices=(
         ("male",u"男(male)"),
         ("female",u"女(female)")
-        ),max_length=INPUT_MAX, blank=True, help_text=u"非必填(not required)")
-    if_still_ntu = models.CharField(verbose_name=u"現在是否為台大在校生(if you're now a NTU student)", choices=(("yes","是(yes)"),("no","否(no)")),max_length=INPUT_MAX)
+        ),max_length=INPUT_MAX, blank=True, help_text=u"非必填(optional)")
+    if_present_ntu = models.BooleanField(verbose_name=u"現在是否為台大在校生(if you're now a NTU student)", default=None, choices=((None,"請選擇"),(True,"是(yes)"),(False,"否(no)")),max_length=INPUT_MAX)
 
     tshirt_size = models.CharField(verbose_name=u"t-shirt尺寸(size)", max_length=INPUT_MAX, choices=(("S","S"),("M","M"), ("L","L"),("XL","XL"),("2XL","2XL"),("3XL","3XL")), help_text=u"<a href=\"http://www.mit-clothes.com.tw/info/size.html\" target=\"_blank\">尺寸表(size chart)</a>")
-    ptt_id = models.CharField(verbose_name=u"PTT ID",max_length=INPUT_MAX, blank=True, help_text=u"非必填(not required)")
-    ptt2_id = models.CharField(verbose_name=u"PTT2 ID",max_length=INPUT_MAX,blank=True, help_text=u"非必填(not required)")
+    ptt_id = models.CharField(verbose_name=u"PTT ID",max_length=INPUT_MAX, blank=True, help_text=u"非必填(optional)")
+    ptt2_id = models.CharField(verbose_name=u"PTT2 ID",max_length=INPUT_MAX,blank=True, help_text=u"非必填(optional)")
 
-    if_vegetarian = models.CharField(verbose_name=u"是否吃素(vegetarian)", choices=(("yes","是(yes)"),("no","否(no)")),max_length=INPUT_MAX )    
+    if_vegetarian = models.BooleanField(verbose_name=u"是否吃素(vegetarian)", default=None, choices=((None,"請選擇"),(True,"是(yes)"),(False,"否(no)")),max_length=INPUT_MAX )    
 
     emergency_contact_name = models.CharField(verbose_name=u"緊急聯絡人姓名(emergency contact)", max_length=INPUT_MAX)
     emergency_contact_mobile = models.CharField(verbose_name=u"緊急聯絡人手機(emergency contact's mobile)", max_length=MOBILE_LEN, help_text=u"不要輸入數字以外的符號(don't enter anything other than numbers)", validators=[RegexValidator(r'^09[0-9]{8}$'),MinLengthValidator(MOBILE_LEN),MaxLengthValidator(MOBILE_LEN)])
@@ -43,76 +43,37 @@ class ntulgUser(models.Model):
 
     beneficiary = models.CharField(verbose_name=u"保險受益人(beneficiary)",max_length=INPUT_MAX) 
     beneficiary_relationship = models.CharField(verbose_name=u"與保險受益人之關係(relationship to beneficiary)",max_length=INPUT_MAX)
-    height = models.DecimalField(verbose_name=u"身高(height)", max_digits=3, help_text="cm")
-    weight = models.DecimalField(verbose_name=u"體重(weight)", max_digits=3, help_text="kg")
-    disease= models.CharField(verbose_name=u"特殊疾病(specified disease)", max_length=INPUT_MAX, blank=True, help_text=u"任何會妨礙訓練的疾病, 非必填(not required)") 
+    height = models.DecimalField(verbose_name=u"身高(height)", max_digits=3, decimal_places=0, help_text=u"cm, 非必填(optional)", blank=True)
+    weight = models.DecimalField(verbose_name=u"體重(weight)", max_digits=3, decimal_places=0, help_text=u"kg, 非必填(optional)", blank=True)
+    disease= models.CharField(verbose_name=u"特殊疾病(specified disease)", max_length=INPUT_MAX, blank=True, help_text=u"任何會妨礙訓練的疾病, 非必填(optional)") 
 
 
-    source = models.CharField(verbose_name=u"如何得知本訓練(...)", max_length=INPUT_MAX)
+    source = models.CharField(verbose_name=u"如何得知本訓練(what brings you here)", max_length=INPUT_MAX)
 
-    comment = models.TextField(verbose_name=u"建議/發問(comment)", max_length=INPUT_MAX,blank=True, help_text=u"非必填(not required)")
+    comment = models.TextField(verbose_name=u"建議/發問(comment)", max_length=INPUT_MAX,blank=True, help_text=u"非必填(optional)")
 
-    facebook_id = models.CharField(verbose_name=u"Facebook ID", max_length=INPUT_MAX,blank=True, help_text=u"全為數字,非必填(all numbers, not required), <a href=\"http://findmyfacebookid.com\" target=\"_blank\">找ID(Help)</a>", validators=[RegexValidator(r'[0-9]'),MinLengthValidator(6),MaxLengthValidator(20)])  
-
-
-class ntulgUserForm(ModelForm):
-    class Meta:
-        model = ntulgUser
-        fields = [
-                'stage_no',
-                'name_cht',
-                'name_eng',
-                'nationality',
-                'identify_number',
-                'birthday',
-                'sex',
-                'tel_mobile',
-                'tel',
-                'email',
-                'address',
-                'height',
-                'weight',
-                'tshirt_size',
-                'if_still_ntu',
-                'if_vegetarian',
-                'emergency_contact_name',
-                'emergency_contact_mobile',
-                'beneficiary',
-                'beneficiary_relationship',
-                'disease',
-                'source',
-                'comment',
-                'facebook_id',
-                'ptt_id',
-                'ptt2_id',
-                ]
-        widgets = { 
-        'address': forms.TextInput(attrs={'size': 80})
-        }
-        
+    facebook_id = models.CharField(verbose_name=u"Facebook ID", max_length=INPUT_MAX,blank=True, help_text=u"全為數字,非必填(all numbers, optional), <a href=\"http://findmyfacebookid.com\" target=\"_blank\">找ID(Help)</a>", validators=[RegexValidator(r'[0-9]'),MinLengthValidator(6),MaxLengthValidator(20)])  
+       
 class ntulgOldUserForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(ntulgUserForm, self).__init__(*args, **kwargs)
-        self.fields["stage_no"].label = "救生班期數(stage no.)"
-
     class Meta:
         model = ntulgUser
         fields = [
                 'stage_no',
+                'cap_no',
                 'name_cht',
                 'name_eng',
                 'nationality',
                 'identify_number',
+                'email',
                 'birthday',
                 'sex',
                 'tel_mobile',
                 'tel',
-                'email',
                 'address',
                 'height',
                 'weight',
                 'tshirt_size',
-                'if_still_ntu',
+                'if_present_ntu',
                 'if_vegetarian',
                 'emergency_contact_name',
                 'emergency_contact_mobile',
@@ -126,15 +87,40 @@ class ntulgOldUserForm(ModelForm):
                 'ptt2_id',
                 ]
         widgets = { 
-        'address': forms.TextInput(attrs={'size': 30})
+        'address': forms.TextInput(attrs={'size': 50})
         }
-        
-class ntulgOldUserForm(ntulgUserForm):
-    def __init__(self, *args, **kwargs):
-        super(ntulgUserForm, self).__init__(*args, **kwargs)
-        self.fields["stage_no"].label = "救生班期數(stage no.)"
-
-class ntulgNewUserForm(ntulgUserForm):
-    def __init__(self, *args, **kwargs):
-        super(ntulgUserForm, self).__init__(*args, **kwargs)
-        self.fields["stage_no"].label = "報名救生班期數(stage no.)"
+ 
+class ntulgNewUserForm(ModelForm):
+    class Meta:
+        model = ntulgUser
+        fields = [
+                'name_cht',
+                'name_eng',
+                'nationality',
+                'identify_number',
+                'email',
+                'birthday',
+                'sex',
+                'tel_mobile',
+                'tel',
+                'address',
+                'height',
+                'weight',
+                'tshirt_size',
+                'if_present_ntu',
+                'if_vegetarian',
+                'emergency_contact_name',
+                'emergency_contact_mobile',
+                'beneficiary',
+                'beneficiary_relationship',
+                'disease',
+                'source',
+                'comment',
+                'facebook_id',
+                'ptt_id',
+                'ptt2_id',
+                ]
+        widgets = { 
+        'address': forms.TextInput(attrs={'size': 50})
+        }
+ 
--- ntulgapp/views.py
+++ ntulgapp/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth import authenticate, login
 import logging
 from ntulgapp.user import ntulgNewUserForm
 from ntulgapp.user import ntulgOldUserForm
+from ntulgapp.globals import CURRENT_STAGE_NO
+from ntulgapp.globals import CURRENT_STAGE_MANAGER
+from ntulgapp.globals import CURRENT_STAGE_DATE
 
 USER_INPUT_LEN_MIN = 1
 USER_INPUT_LEN_MAX = 100
@@ -16,7 +19,7 @@ USER_INPUT_LEN_MAX = 100
 logger = logging.getLogger(__name__)
 
 class loginForm(forms.Form):
-    login_id = forms.CharField(required=False, label=u'帳號(account)', help_text=u'你的身份證字號(your ID)', max_length=USER_INPUT_LEN_MAX)
+    login_id = forms.CharField(required=False, label=u'帳號(account)', help_text=u'你的身份證字號(your ID.)', max_length=USER_INPUT_LEN_MAX)
     login_pw = forms.CharField(required=False, label=u'密碼(password)', max_length=USER_INPUT_LEN_MAX)
 
 class signupForm(forms.Form):
@@ -38,6 +41,7 @@ def signup_view(request):
         if u"confirm" in post_keys:
             logging.info(request.POST)
             if form.is_valid(): # All validation rules pass
+                n = form.save()
                 return HttpResponse('ok login')
             else:
                 return returnError(request, form, u'資料輸入有誤，請檢查欄位，完成後請按\"確定\"！')
@@ -51,40 +55,55 @@ def signup_view(request):
 
 def signup_new_view(request):
     def returnError(request, form, error):
-        return render_to_response('signup_new.html',
-                {
-                    'form': form,
-                    'error': error,
-                    },
-                context_instance=RequestContext(request)
-                )
-   
+        return render_to_response('signup_new.html', {
+            'form': form,
+            'error': error,
+            'stage_no': CURRENT_STAGE_NO,
+            'stage_date': CURRENT_STAGE_DATE,
+            'stage_manager': CURRENT_STAGE_MANAGER
+            },
+            context_instance=RequestContext(request)
+            )
+
     if request.method == 'POST': # If the form has been submitted...
         form = ntulgNewUserForm(request.POST) # A form bound to the POST data
         post_keys = request.POST.keys()
         if u"confirm" in post_keys:
             logging.info(request.POST)
+            logging.info(dir(form))
+            logging.info(form.fields)
+            logging.info(form.base_fields)
             if form.is_valid(): # All validation rules pass
+                n = form.save()
                 return HttpResponse('ok login')
             else:
                 return returnError(request, form, u'資料輸入有誤，請檢查欄位，完成後請按\"確定\"！')
         elif u"cancel" in post_keys:
             #return HttpResponse('sign up')
             return render(request, 'home.html', {
-                'form': loginForm})
+                'form': loginForm,
+                'stage_no': CURRENT_STAGE_NO,
+                'stage_date': CURRENT_STAGE_DATE,
+                'stage_manager': CURRENT_STAGE_MANAGER})
 
     else:
-        form = ntulgNewUserForm(initial={'stage_no': 29}) # An unbound form
+        form = ntulgNewUserForm() # An unbound form
 
-    return render(request, 'signup_new.html', {'form': form})
+    return render(request, 'signup_new.html', {
+        'form': form,
+        'stage_no':CURRENT_STAGE_NO,
+        'stage_date': CURRENT_STAGE_DATE,
+        'stage_manager': CURRENT_STAGE_MANAGER})
 
 
 def login_view(request):
     if request.method == 'POST': # If the form has been submitted...
         form = loginForm(request.POST) # A form bound to the POST data
         post_keys = request.POST.keys()
+        logging.info(post_keys)
         if u"signin" in post_keys:
             if form.is_valid(): # All validation rules pass
+                logging.info("valid")
                 login_id = request.POST['login_id']
                 login_pw = request.POST['login_pw']
 
@@ -95,6 +114,9 @@ def login_view(request):
                         return HttpResponse('ok login') 
                 else:
                     return HttpResponse('bad login')
+            else:
+                logging.info("valid")
+                return HttpResponse('bad login')
         elif u"signup" in post_keys:
             #return HttpResponse('sign up')
             return render(request, 'signup.html', {
@@ -103,4 +125,6 @@ def login_view(request):
     else:
         form = loginForm() # An unbound form
 
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html', {
+        'form': form,
+        'disabled':''})
