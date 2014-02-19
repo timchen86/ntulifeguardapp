7e40117586f688b3b83f5d9489651050dd214007
ctf <tim.chen.86@gmail.com>
2014-02-19 16:42:12 +0800

    ignore secret file

--- ntulifeguardapp/globals.py
+++ ntulifeguardapp/globals.py
@@ -7,5 +7,3 @@ CURRENT_STAGE = {
 APP_URL="http://ntulifeguardapp.appspot.com/"
 APP_ADMIN_EMAIL="ntulifeguard@gmail.com"
 APP_NOTICE_EMAIL="ntulifeguarddoc@groups.facebook.com"
-
-APP_GD_SECRET = {"web":{"auth_uri":"https://accounts.google.com/o/oauth2/auth","client_secret":"gGss9W9-3xPXcde4swMrBqJv","token_uri":"https://accounts.google.com/o/oauth2/token","client_email":"1005762717201@developer.gserviceaccount.com","client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/1005762717201@developer.gserviceaccount.com","client_id":"1005762717201.apps.googleusercontent.com","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs"}}
